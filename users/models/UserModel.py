1b6f39b689945b14d06b6c7a86b10a98710a3d42
Mercurial5 <dias.nespayev@gmail.com>
2023-01-11 16:57:37 +0600

    init

---
+++ users/models/UserModel.py
@@ -0,0 +1,57 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+from users import models as my_models
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **kwargs):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, email, password, **kwargs):
+        kwargs.setdefault('is_staff', False)
+        kwargs.setdefault('is_superuser', False)
+
+        return self._create_user(email, password, **kwargs)
+
+    def create_superuser(self, email, password, **kwargs):
+        kwargs.setdefault('is_staff', True)
+        kwargs.setdefault('is_superuser', True)
+
+        if kwargs.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True')
+        if kwargs.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+
+        return self._create_user(email, password, **kwargs)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = None
+    email = models.EmailField(unique=True)
+
+    role = models.OneToOneField(my_models.Role, on_delete=models.PROTECT, null=True, blank=True)
+    city = models.OneToOneField(my_models.City, on_delete=models.PROTECT, null=True, blank=True)
+
+    phone = models.CharField(max_length=15)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}'
---
+++ users/models/__init__.py
@@ -0,0 +1,3 @@
+from users.models.CityModel import City
+from users.models.RoleModel import Role
+from users.models.UserModel import User
