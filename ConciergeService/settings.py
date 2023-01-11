3aaa1e7f52ebd79b83b3468e84c586b4a1bfd9be
Mercurial5 <dias.nespayev@gmail.com>
2023-01-11 21:37:11 +0600

    fix: send activation mail

--- ConciergeService/settings.py
+++ ConciergeService/settings.py
@@ -64,6 +64,7 @@ ROOT_URLCONF = 'ConciergeService.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
