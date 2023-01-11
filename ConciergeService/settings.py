8d339fd04c87ff49c62125fffeda2fdf54daf4d9
Mercurial5 <dias.nespayev@gmail.com>
2023-01-11 17:07:41 +0600

    fix: add `TEMPLATES` for admin panel

--- ConciergeService/settings.py
+++ ConciergeService/settings.py
@@ -60,6 +60,20 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'ConciergeService.urls'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 WSGI_APPLICATION = 'ConciergeService.wsgi.application'
 
 # Database
