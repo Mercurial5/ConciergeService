aab7da197320c93f41f2b89f9d17905f10e7c791
Mercurial5 <dias.nespayev@gmail.com>
2023-01-12 17:55:01 +0600

    fix: user creation error

--- users/models/UserModel.py
+++ users/models/UserModel.py
@@ -51,7 +51,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['role', 'city']
 
     def __str__(self):
         return f'{self.email}'
