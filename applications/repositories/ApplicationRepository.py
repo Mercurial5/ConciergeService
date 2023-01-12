5f0eea21791812f4346b1ef908eacfa7a703a2e3
Mercurial5 <dias.nespayev@gmail.com>
2023-01-12 18:23:53 +0600

    chore: use only owner_id when creating application

--- applications/repositories/ApplicationRepository.py
+++ applications/repositories/ApplicationRepository.py
@@ -21,7 +21,6 @@ class ApplicationRepository:
 
     def create(self, **kwargs) -> models.Application:
         owner_id = kwargs['owner_id']
-        manager_id = kwargs['manager_id']
 
         try:
             owner = self.user_service.get(owner_id)
@@ -31,15 +30,7 @@ class ApplicationRepository:
         if owner.role.name not in ('client', 'company'):
             raise exceptions.NotClientOrCompany(f'User with id {owner_id} is not a client or a company.')
 
-        try:
-            manager = self.user_service.get(manager_id)
-        except UserDoesNotExist:
-            raise exceptions.ManagerDoesNotExist(f'Manager with id {manager_id} does not exist.')
-
-        if manager.role.name != 'manager':
-            raise exceptions.NotManager(f'User with id {manager_id} is not a manager')
-
-        return self.model.objects.create(**kwargs)
+        return self.model.objects.create(owner_id=owner_id)
 
     def get_list(self) -> QuerySet[models.Application]:
         return self.model.objects.all()
