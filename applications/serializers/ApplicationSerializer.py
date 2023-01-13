009e48d70db4ed9e5d262f23a3160ffa81c0c4d5
Mercurial5 <dias.nespayev@gmail.com>
2023-01-13 15:23:40 +0600

    feat: add new serializer to create applications

--- applications/serializers/ApplicationSerializer.py
+++ applications/serializers/ApplicationSerializer.py
@@ -1,9 +1,27 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from applications import models
 
 
+class _ApplicationUserField(serializers.ModelSerializer):
+    role = serializers.CharField(source='role.name')
+
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'email', 'role']
+
+
 class ApplicationSerializer(serializers.ModelSerializer):
+    owner = _ApplicationUserField(read_only=True)
+    manager = _ApplicationUserField(read_only=True)
+
+    class Meta:
+        model = models.Application
+        fields = '__all__'
+
+
+class ApplicationCreateSerializer(serializers.ModelSerializer):
     owner = serializers.IntegerField(source='owner_id')
 
     class Meta:
