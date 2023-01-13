7b87b2051f7005b32294ac943d18eeaf73ecd2b3
Mercurial5 <dias.nespayev@gmail.com>
2023-01-13 20:24:40 +0600

    chore: or I did...

--- applications/serializers/__init__.py
+++ applications/serializers/__init__.py
@@ -1,3 +1,4 @@
 from applications.serializers.ApplicationSerializer import ApplicationCreateSerializer
 from applications.serializers.ApplicationSerializer import ApplicationPartiallyUpdateSerializer
 from applications.serializers.ApplicationSerializer import ApplicationSerializer
+from applications.serializers.ServiceSerializer import ServiceSerializer
--- applications/services/__init__.py
+++ applications/services/__init__.py
@@ -1,1 +1,2 @@
 from applications.services.ApplicationService import ApplicationServiceInterface, ApplicationService
+from applications.services.ServiceService import ServiceServiceInterface, ServiceService
