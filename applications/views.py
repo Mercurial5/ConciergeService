3e43eaf5888e0930fe4a6663ba5ead8261f35d40
Mercurial5 <dias.nespayev@gmail.com>
2023-01-13 15:02:28 +0600

    feat: use custom create for applications

--- applications/views.py
+++ applications/views.py
@@ -1,7 +1,9 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
-from applications import serializers, services
+from applications import serializers, services, exceptions
 from users.services import UserService
 
 
@@ -12,3 +14,14 @@ class ApplicationViewSet(ModelViewSet):
 
     def get_queryset(self):
         return self.service.get_list()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        try:
+            self.service.create(**serializer.validated_data)
+        except exceptions.ApplicationException as e:
+            return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
