38900b3cf8998229bd72e5ecd2da631048b97326
Mercurial5 <dias.nespayev@gmail.com>
2023-01-20 12:12:25 +0600

    chore: change auth/ to api/auth/

--- ConciergeService/urls.py
+++ ConciergeService/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('djoser.urls.authtoken')),
+    path('api/auth/', include('djoser.urls.authtoken')),
 
     path('api/users/', include('users.urls')),
     path('api/applications/', include('applications.urls')),
