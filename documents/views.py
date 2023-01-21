17a82d4ae7d985287e5c7c39c0d8161ddf9afa8b
Mercurial5 <dias.nespayev@gmail.com>
2023-01-21 20:38:29 +0600

    chore: remove permissions

--- documents/views.py
+++ documents/views.py
@@ -1,13 +1,13 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from documents import serializers, models, permissions
+from documents import serializers, models
 
 
 class DocumentViewSet(viewsets.ModelViewSet):
     queryset = models.Document.objects.all()
     serializer_class = serializers.DocumentSerializer
-    permission_classes = [IsAuthenticated, permissions.IsRelated]
+    permission_classes = [IsAuthenticated]
 
 
 class DocumentTypeViewSet(viewsets.ModelViewSet):
