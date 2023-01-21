d76286a0525d481e300e337c2ac599604d28bf69
Mercurial5 <dias.nespayev@gmail.com>
2023-01-21 19:58:32 +0600

    feat: add document types

--- documents/models.py
+++ documents/models.py
@@ -4,8 +4,13 @@ from django.db import models
 User = get_user_model()
 
 
+class DocumentType(models.Model):
+    name = models.CharField(max_length=255)
+
+
 class Document(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.ForeignKey(DocumentType, on_delete=models.PROTECT)
 
     name = models.CharField(max_length=255)
     document = models.FileField(upload_to='documents/docs/')
