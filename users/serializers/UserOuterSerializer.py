161ed8a2d24545b0806156f4ee034672b30f7408
Mercurial5 <dias.nespayev@gmail.com>
2023-01-21 23:14:12 +0600

    fix: use id instead of name

--- users/serializers/UserOuterSerializer.py
+++ users/serializers/UserOuterSerializer.py
@@ -3,9 +3,6 @@ from users import models
 
 
 class UserOuterSerializer(serializers.ModelSerializer):
-    role = serializers.CharField(source='role.name')
-    city = serializers.CharField(source='city.name')
-
     class Meta:
         model = models.User
         fields = ['id', 'email', 'name', 'surname', 'role', 'city']
