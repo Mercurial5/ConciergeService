cb7fe049340408fd7edc4682b7c3db666b444fd3
Mercurial5 <dias.nespayev@gmail.com>
2023-01-26 12:46:01 +0600

    fix: remove IsAuthenticated

--- applications/views/ServiceCategoryView.py
+++ applications/views/ServiceCategoryView.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
 
 from applications import models, serializers
 
@@ -7,4 +6,3 @@ from applications import models, serializers
 class ServiceCategoryView(viewsets.ModelViewSet):
     queryset = models.ServiceCategory.objects.all()
     serializer_class = serializers.ServiceCategorySerializer
-    permission_classes = [IsAuthenticated]
