4f43be95a59c27425b31726f9710efa5f8f7954a
Mercurial5 <dias.nespayev@gmail.com>
2023-01-29 20:55:25 +0600

    feat: return last message of chat

--- chat/serializers.py
+++ chat/serializers.py
@@ -7,6 +7,12 @@ class ChatSerializer(serializers.ModelSerializer):
         model = models.Chat
         fields = '__all__'
 
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        last_message = models.Message.objects.filter(chat_id=data['id']).order_by('id').last()
+        data['last_message'] = MessageSerializer(last_message).data
+        return data
+
 
 class MessageTypeSerializer(serializers.ModelSerializer):
     class Meta:
