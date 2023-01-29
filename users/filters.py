223fc57cb547c7d43f9001669ac96bd56c4f5f18
Mercurial5 <dias.nespayev@gmail.com>
2023-01-30 00:05:28 +0600

    chore: filter by id

--- users/filters.py
+++ users/filters.py
@@ -1,11 +1,9 @@
 from django_filters import rest_framework as filters
+
 from users import models
 
 
 class UsersFilter(filters.FilterSet):
-    # partner = filters.CharFilter(field_name='partner__name', lookup_expr='iexact')
-    role = filters.CharFilter(field_name='role__name')
-
     class Meta:
         model = models.User
         fields = ['role']
