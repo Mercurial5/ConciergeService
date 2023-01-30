e8ba91dfe733cf0c6793d3db1c48f83bf1071d6f
Mercurial5 <dias.nespayev@gmail.com>
2023-01-30 19:35:55 +0600

    feat: add pagination

--- ConciergeService/settings.py
+++ ConciergeService/settings.py
@@ -139,6 +139,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 12
 }
 
 DJOSER = {
@@ -177,5 +179,3 @@ CHANNEL_LAYERS = {
 }
 
 ASGI_APPLICATION = 'chat.routing.application'
-
-
