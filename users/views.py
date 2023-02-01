82f11ae86caaa5d8201f6ca3c3283c6d1b426fcc
Mercurial5 <dias.nespayev@gmail.com>
2023-02-01 16:11:39 +0600

    chore: modify users pagination class

--- users/views.py
+++ users/views.py
@@ -18,6 +18,15 @@ class StandardResultsSetPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 10
 
+    def get_paginated_response(self, data):
+        return Response({
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'results': data
+        })
+
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserSerializer
